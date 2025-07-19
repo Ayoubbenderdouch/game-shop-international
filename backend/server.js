efb99dbe774416bb2c412a0c00465e4960eb536d
youcef mhammdi bouzina <youcef.mhammdi.bouzina@ensia.edu.dz>
2025-07-19 19:07:56 +0100

    commit

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.ADMIN_DASHBOARD_URL],
+    origin: [process.env.FRONTEND_URL, process.env.ADMIN_DASHBOARD_URL,"https://game-shop-wine.vercel.app"],
     credentials: true,
   })
 );
